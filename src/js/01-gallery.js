f893bcde0cad809066e31ec5f1a7a5af933c4566
Oleksandr Kutsenko <104622826+Fstv-Electro@users.noreply.github.com>
2022-11-08 14:26:09 +0200

    hw-07

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,51 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryDiv = document.querySelector(".gallery");
+
+const galleryMarkup = makeGalleryMarkup(galleryItems);
+
+galleryDiv.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryDiv.addEventListener("click", onGalleryClick);
+
+function makeGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link href="${original}">
+    <img class="gallery__image" src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    /></a></div>`;
+    })
+    .join("");
+}
+
+function onGalleryClick(evt) {
+  evt.preventDefault();
+  const url = evt.target.classList.contains("gallery__image");
+  if (!url) {
+    return;
+  }
+
+  const originalImg = evt.target.dataset.source;
+
+  const modal = basicLightbox.create(`<img src="${originalImg}">`, {
+    onShow: () => {
+      document.addEventListener("keydown", onEscape);
+    },
+    onClose: () => {
+      document.removeEventListener("keydown", onEscape);
+    },
+  });
+
+  modal.show();
+
+  function onEscape(evt) {
+    if (evt.code !== "Escape") {
+      return;
+    }
+    modal.close();
+  }
+}
